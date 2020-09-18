3f535882a76bab3f78a8c349127a7dc5f7bb50bc
wesleyyee1996 <wesleyyee1996@gmail.com>
2020-09-18 17:47:38 -0500

    commit

--- Lab0/Problem4.m
+++ Lab0/Problem4.m
@@ -17,7 +17,7 @@ theta5 = pi/2;
 %DH Parameters
 DH_params = [0, -pi/2, L1, theta1; 
             L2, 0, 0, theta2 + pi/2;
-            L3, 0, 0, theta3 + pi/2
+            L3, 0, 0, theta3 + pi/2;
             0, pi/2, 0, theta4 - pi/2;
             0, 0, L4+L5, theta5 + pi];
         
@@ -27,7 +27,7 @@ A = zeros(4);
        
 %Initialize T matrices, combined them into a single matrix to make easier
 %to calculate
-T = zeros(20,4);
+T = eye(4);
 
 %Calculate each intermediate homogeneous transformation matrix
 for link = 1:size(DH_params, 1)
@@ -36,24 +36,12 @@ for link = 1:size(DH_params, 1)
     d = DH_params(link,3);
     theta = DH_params(link,4);
     
-    %Calculate DH Transformation Matrix A
-    A = [cos(theta), -sin(theta)*cos(alpha), sin(theta)*sin(alpha), a*cos(theta);
-        sin(theta), cos(theta)*cos(alpha), -cos(theta)*sin(alpha), a*sin(theta);
-        0, sin(alpha), cos(alpha), d;
-        0, 0, 0, 1];
-    
-    %Replace section in T with A
-    T(4*link-3:4*link,1:4) = A;
-end;
+    % calculate A
+    A = createA(a, alpha, d, theta);
+    disp('T'+link+' = ');
+    disp(A);
+    T = T*A; 
+end
+disp('T0e = ');
+disp(T);
 
-%Pull out T's and set them to their own variables
-T01 = T(1:4, 1:4);
-T12 = T(5:8, 1:4);
-T23 = T(9:12, 1:4);
-T34 = T(13:16, 1:4);
-T45 = T(17:20, 1:4);
-    
-%Calculate overall homogeneous matrix
-T_final = T01*T12*T23*T34*T45;
-
-disp(T_final);
