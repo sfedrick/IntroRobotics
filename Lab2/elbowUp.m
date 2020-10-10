3ebeaaee7f8129d74a0c290144eeb86adf87e376
unknown <shaunclfedrick@gmail.com>
2020-10-09 20:12:06 -0400

    trying

--- Lab2/elbowUp.m
+++ Lab2/elbowUp.m
@@ -11,17 +11,18 @@ function [theta1,theta2,theta3] = elbowUp(C, constants)
     a2 = constants(2);
     a3 = constants(3);
 
-
+%{
     theta1 = atan2(y0c,x0c);
     r1 = sqrt(y0c^2 + x0c^2);
     r2 = z0c - d1;
     phi2 = atan2(r2,r1);
+    %r3 is just (x^2 + y^2 +z^2)^0.5 
     r3 = sqrt(r1^2 + r2^2);
     phi1 = acos((a3^2-a2^2-r3^2)/(-2*a2*r3));
     theta2 = phi2-phi1;
     phi3 = acos((r3^2-a2^2-a3^2)/(-2*a2*a3));
     theta3 = pi/2-phi3;
-%{
+%}
     l2 = a2 + a1;
     r = sqrt(x0c^2 + y0c^2);
     theta1 = atan2( y0c,x0c);
@@ -32,6 +33,6 @@ function [theta1,theta2,theta3] = elbowUp(C, constants)
     alpha = atan2((l2+a3*cos(omega)),(a3*sin(omega)));
     beta = atan2(r,z0c);
     theta2 = pi/2 - alpha - beta;
-    %}
+    
 end
 
