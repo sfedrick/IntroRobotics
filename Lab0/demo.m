0855d51430d0c31676ea8b14b4d4d26f166ba621
unknown <shaunclfedrick@gmail.com>
2020-10-11 18:53:51 -0400

    this all works consider adding check that determines if position joints have valid rotation joints

--- Lab0/demo.m
+++ Lab0/demo.m
@@ -9,7 +9,7 @@ con = rosStart(true); % start ROS with gripper enabled
 % add command
 q=[0   , 1.0591-pi/2 ,  -1,  -0.2387     , 0, 5];
 %q=[pi/4,0,0,0,0,0];
-%q = [0,0,0, 0,0,0];
+q = [0,0,0, 0,0,0];
 
 con = add_command(con,q);
 disp("Now the current state value is : ...");
