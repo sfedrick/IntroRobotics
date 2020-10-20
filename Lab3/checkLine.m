f8585b6961e9da276b280677359684aed1f8437e
wesleyyee1996 <wesleyyee1996@gmail.com>
2020-10-20 00:44:36 -0500

    added more classes for lab3 RRT

--- Lab3/checkLine.m
+++ Lab3/checkLine.m
@@ -1,8 +1,18 @@
-function [OkLine] = checkLine(line)
+function [OkLine] = checkLine(P, node, obstacles)
 %UNTITLED5 Summary of this function goes here
 %   Detailed explanation goes here
+
+
 %checks that each point on the line has an inverse kinematic solution if it
 %doesn't the line is not okay
+checkIKvalid(P, node, )
+
+% create a discretized line in 3D space from the closest node in start tree --> point P
+% and closest node in end tree to point P
+
+%checks for collisions
+checkCollision(line
+
 OkLine=true;
 end
 
