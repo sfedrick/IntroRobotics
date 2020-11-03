a04ee2953be15d51e62078eec265070d529321a7
wesleyyee1996 <wesleyyee1996@gmail.com>
2020-11-02 18:10:29 -0600

    added plotting function

---
+++ Lab4/linePlot.m
@@ -0,0 +1,18 @@
+function [] = linePlot(point1, point2, hasLine, color, width)
+% Plots line between 2 nodes and also plots the 2nd node
+
+hold on;
+
+if (hasLine == true)
+    line = point1-point2;
+    step = line/1000;
+    
+    v1=point1;
+    v2=point2;
+    v=[v2;v1];
+    plot3(v(:,1),v(:,2),v(:,3),'Color',color,'linewidth',width);
+
+end
+
+end
+
---
+++ Lab4/trajPlot.m
@@ -0,0 +1,23 @@
+function [] = trajPlot(T)
+%PLOT plots a given homogeneous matrix in 3D space
+
+% Extract rotation and position matrices
+R = T(1:3,1:3);
+d = T(1:3,4)';
+
+% Construct x, y, z lines starting at each point
+x = R(1:3,1)';
+y = R(1:3,2)';
+z = R(1:3,3)';
+
+hold on;
+% Plot actual point in space
+plot3(d(1),d(2),d(3),'.k','MarkerSize',30); 
+
+% Plots the axes
+linePlot(d,x+d,1,[1,0,0],4); % x = red
+linePlot(d,y+d,1,[0,1,0],4); % y = green
+linePlot(d,z+d,1,[0,0,1],4); % z = blue
+
+end
+
