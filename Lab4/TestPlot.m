bd33ce5bb7dbe976a529f4ebd021616c2850c4df
unknown <shaunclfedrick@gmail.com>
2020-11-06 19:29:44 -0500

    compute singular configs

---
+++ Lab4/TestPlot.m
@@ -0,0 +1,18 @@
+function [] = TestPlot(nodeOld, nodeNew, hasLine, color, width)
+% Plots line between 2 nodes and also plots the 2nd node
+
+hold on;
+plot3(nodeNew(1),nodeNew(2),nodeNew(3),'.k','MarkerSize',30); 
+if (hasLine == true)
+    line = nodeOld-nodeNew;
+    step = line/1000;
+    
+    v1=nodeOld;
+    v2=nodeNew;
+    v=[v2;v1];
+    plot3(v(:,1),v(:,2),v(:,3),'Color',color,'linewidth',width);
+
+end
+
+end
+
--- Lab4/computeSingularities.m
+++ Lab4/computeSingularities.m
@@ -1,7 +1,7 @@
 % Fill in this file with your code for Analysis question #4.
 
 % # Discretizations for each joint variable
-discStep = 25;
+discStep = 10;
 
 %Link Lengths
 L1 = 76.2;
@@ -20,23 +20,32 @@ z = [];
 % Compute the end effector Cartesian coordinate for each permutation of 
 % joint variable
 joint=6;
+lowerLim = [-1.4, -1.2, -1.8, -1.9, -2.0, -15]; % Lower joint limits in radians (grip in mm (negative closes more firmly))
+upperLim = [ 1.4,  1.4,  1.7,  1.7,  1.5,  30]; % Upper joint limits in radians (grip in mm)
+dims = [lowerLim;upperLim]';
+random=100000;
+close=0.05;
+closerandom=20;
 hold on;
-for joint1 = -1.4:2.8/discStep:1.4
-    for joint2 = -1.2:2.6/discStep:1.4
-        for joint3 = -1.4:3.1/discStep:1.7
-            for joint4 = -1.9:3.6/discStep:1.7
-                for joint5 = -2:3.5/discStep:1.5
-                   qconfig=[joint1 ,joint2,joint3,joint4,joint5];
-                   J=CreateJac(qconfig,joint);
-                   J=round(J);
-                   if(rank(J)<5)
-                       print("percent")
-                       
-                        [jointPositions,T0e]=calculateFK(qconfig);
-                        x=jointPositions(joint,:);
-                        plot3(x(1),x(2),x(3),'-o','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF');
-                   end 
-                end
+plotJointPos([0,0,0,0,0,0], [0.5,0,0],2)
+for i=1:random
+  qconfig=randpoint(dims);
+  J=CreateJac(qconfig,joint);
+  J=round(J);
+ 
+    if(rank(J)<5)
+         closelow=qconfig-close;
+         closeup=qconfig+close;
+         closedim=[closelow;closeup]';
+        for j=1:closerandom
+            qconfig=randpoint(closedim);
+            J=CreateJac(qconfig,joint);
+            J=round(J);
+            if(rank(J)<5)
+                %plotJointPos(qconfig, [0.5,0,0],2)
+                [jointPositions,T0e]=calculateFK(qconfig);
+                x=jointPositions(joint,:);
+                plot3(x(1),x(2),x(3),'-o','Color','b','MarkerSize',10,'MarkerFaceColor','#D9FFFF');
             end
         end
     end
@@ -44,4 +53,6 @@ end
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
+axis equal;
+axis([-260 260 -260 260 -400 400]);
 hold off;
